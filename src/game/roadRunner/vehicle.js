c15541c44380c4d5405301a6ddab57f237b8bc03
Hunting-Fishing <103216816+Hunting-Fishing@users.noreply.github.com>
2026-06-14 02:38:04 +0800

    Render Road Runner vehicles from image assets

--- src/game/roadRunner/vehicle.js
+++ src/game/roadRunner/vehicle.js
@@ -1,17 +1,25 @@
-export function createVehicle(scene, color = 0xf97316, label = '') {
+export function createVehicle(scene, color = 0xf97316, label = '', textureKey = null) {
   const node = scene.add.container(80, 500);
-  node.add(scene.add.ellipse(0, 17, 74, 18, 0x000000, 0.20));
-  node.add(scene.add.roundRectangle(0, 0, 62, 26, 9, color).setStrokeStyle(3, 0x172033));
-  node.add(scene.add.roundRectangle(-5, -8, 30, 18, 5, 0xfef3c7).setStrokeStyle(2, 0x172033));
+  node.add(scene.add.ellipse(0, 20, 82, 18, 0x000000, 0.20));
 
-  const frontWheel = scene.add.circle(21, 16, 8, 0x111827).setStrokeStyle(2, 0xcbd5e1);
-  const rearWheel = scene.add.circle(-22, 16, 8, 0x111827).setStrokeStyle(2, 0xcbd5e1);
-  node.add([frontWheel, rearWheel]);
-  node.frontWheel = frontWheel;
-  node.rearWheel = rearWheel;
+  if (textureKey && scene.textures.exists(textureKey)) {
+    const image = scene.add.image(0, 0, textureKey);
+    image.setOrigin(0.5, 0.5);
+    image.setDisplaySize(96, 48);
+    node.add(image);
+    node.assetImage = image;
+  } else {
+    node.add(scene.add.roundRectangle(0, 0, 62, 26, 9, color).setStrokeStyle(3, 0x172033));
+    node.add(scene.add.roundRectangle(-5, -8, 30, 18, 5, 0xfef3c7).setStrokeStyle(2, 0x172033));
+    const frontWheel = scene.add.circle(21, 16, 8, 0x111827).setStrokeStyle(2, 0xcbd5e1);
+    const rearWheel = scene.add.circle(-22, 16, 8, 0x111827).setStrokeStyle(2, 0xcbd5e1);
+    node.add([frontWheel, rearWheel]);
+    node.frontWheel = frontWheel;
+    node.rearWheel = rearWheel;
+  }
 
   if (label) {
-    node.add(scene.add.text(-30, -36, label, {
+    node.add(scene.add.text(-32, -42, label, {
       fontSize: '11px',
       color: '#ffffff',
       fontStyle: 'bold',
