d040fbd7e4338a5af6552c15e36ec09ae439b96e
Hunting-Fishing <103216816+Hunting-Fishing@users.noreply.github.com>
2026-06-14 02:38:18 +0800

    Render Road Runner pickups from image assets

--- src/game/roadRunner/pickups.js
+++ src/game/roadRunner/pickups.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import { routeY } from './terrain.js';
+import { ROAD_RUNNER_ASSETS } from './assets.js';
 
 export function createPickups(scene, route) {
   const pickups = [];
@@ -7,17 +8,26 @@ export function createPickups(scene, route) {
   for (let x = 220; x < route.length; x += 130) {
     const type = x % 390 === 0 ? 'energy' : x % 260 === 0 ? 'parts' : 'coin';
     const y = routeY(route, x) - 55 - ((x / 130) % 3) * 15;
-    const color = type === 'energy' ? 0x38bdf8 : type === 'parts' ? 0xa78bfa : 0xfacc15;
+    const fallbackColor = type === 'energy' ? 0x38bdf8 : type === 'parts' ? 0xa78bfa : 0xfacc15;
     const pickup = scene.add.container(x, y);
+    const asset = ROAD_RUNNER_ASSETS[type];
 
     pickup.type = type;
     pickup.collected = false;
-    pickup.add(scene.add.circle(0, 0, 14, color).setStrokeStyle(3, 0xffffff, 0.65));
-    pickup.add(scene.add.text(-7, -9, type === 'energy' ? 'E' : type === 'parts' ? 'P' : '$', {
-      fontSize: '15px',
-      fontStyle: 'bold',
-      color: '#06131d'
-    }));
+
+    if (asset && scene.textures.exists(asset.key)) {
+      const icon = scene.add.image(0, 0, asset.key);
+      icon.setDisplaySize(32, 32);
+      pickup.add(icon);
+    } else {
+      pickup.add(scene.add.circle(0, 0, 14, fallbackColor).setStrokeStyle(3, 0xffffff, 0.65));
+      pickup.add(scene.add.text(-7, -9, type === 'energy' ? 'E' : type === 'parts' ? 'P' : 'C', {
+        fontSize: '15px',
+        fontStyle: 'bold',
+        color: '#06131d'
+      }));
+    }
+
     pickups.push(pickup);
   }
 
