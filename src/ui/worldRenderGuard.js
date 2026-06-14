be5ef6ae24a8ae0bde26315aa1e545c60f522642
Hunting-Fishing <103216816+Hunting-Fishing@users.noreply.github.com>
2026-06-14 10:33:36 +0800

    Stop app loop from rerendering Road Runner race screen

--- src/ui/worldRenderGuard.js
+++ src/ui/worldRenderGuard.js
@@ -2,7 +2,7 @@ const nativeSetTimeout = window.setTimeout.bind(window);
 
 window.setTimeout = function guardedSetTimeout(callback, delay, ...args) {
   const worldIsActive = () => Boolean(document.querySelector('#screen-world.active'));
-  const raceIsActive = () => Boolean(document.querySelector('#screen-race.active .roadRunnerShell'));
+  const raceIsActive = () => Boolean(document.querySelector('#screen-race.active'));
   if (delay === 650 && (worldIsActive() || raceIsActive())) {
     return nativeSetTimeout(() => {}, delay);
   }
